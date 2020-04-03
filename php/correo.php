264b9fc2cf4a148f034322912a68bf33355b8efb
hernan juan <chicomuerte35@gmail.com>
2020-04-03 16:11:19 -0600

    Pixel de Facebook

--- php/correo.php
+++ php/correo.php
@@ -5,8 +5,8 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
 $mensaje   = 'Hola Nombre: ' .$name."\nEmail: ".$email."\nTelefono: ".$phone;
-$cabeceras = 'From: diana.alconedo@sentimar.com' . "\r\n" .
-    'Reply-To: diana.alconedo@sentimar.com' . "\r\n" .
+$cabeceras = 'From: sales@sentimar.com' . "\r\n" .
+    'Reply-To: sales@sentimar.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($para, $titulo, $mensaje, $cabeceras);
 header("Location:../gracias.html")
