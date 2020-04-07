8cc1640ffb686b56e3dd3ed02541acab4634976a
DianRouge <rouge.roze.da@gmail.com>
2020-04-06 22:00:31 -0500

    version16

--- php/correo.php
+++ php/correo.php
@@ -1,12 +1,13 @@
 <?php
-$para      = 'sales@sentimar.com';
+$para      = 'ventas@sentimar.com.mx';
 $titulo    = 'Paneles Solares';
 $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
-$mensaje   = 'Hola Nombre: ' .$name."\nEmail: ".$email."\nTelefono: ".$phone;
-$cabeceras = 'From: sales@sentimar.com' . "\r\n" .
-    'Reply-To: sales@sentimar.com' . "\r\n" .
+$comentarios=$_POST['comentarios'];
+$mensaje   = 'Nombre: ' .$name."\nTelefono: ".$phone."\nEmail: ".$email."\nComentarios: ".$comentarios;
+$cabeceras = 'From: ventas@sentimar.com.mx' . "\r\n" .
+    'Reply-To: ventas@sentimar.com.mx' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($para, $titulo, $mensaje, $cabeceras);
 header("Location:../gracias.html")
